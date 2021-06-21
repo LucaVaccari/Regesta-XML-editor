84f61ff367f8d44dd902e0c2441c426bc97ff640
NicolaVivante <57539526+NicolaVivante@users.noreply.github.com>
2021-06-21 18:03:10 +0200

    CJ not working yet

---
+++ js/customJSON.js
@@ -0,0 +1,67 @@
+let attributes = [];
+let customJSON;
+
+function XMLtoCustomJSON(xml) {
+    let doc = new DOMParser().parseFromString(
+        xml.replaceAll(/\n|\t/g, "").replaceAll(/> </g, "><"),
+        "text/xml"
+    );
+
+    let root = doc.getRootNode();
+    console.log(root);
+    console.log(XMLDocToCustomObj(root));
+    let { CJ, noAttributesCJ } = XMLDocToCustomObj(root);
+    customJSON = CJ;
+    return noAttributesCJ;
+
+    function XMLDocToCustomObj(node, id = -1) {
+        let CJ = {}, noAttributesCJ = {};
+
+        switch (node.nodeType) {
+            case Node.TEXT_NODE: // a leaf of the document tree
+                {
+                    return node.nodeValue;
+                }
+            case Node.ELEMENT_NODE: // a branch of the document tree
+                CJ.id = lastId++;
+                noAttributesCJ.id = lastId++;
+                CJ.key = node.nodeName;
+                noAttributesCJ.key = node.nodeName;
+                CJ.value = node.childNodes.length > 1 ? [] : "";
+                noAttributesCJ.value = node.childNodes.length > 1 ? [] : "";
+                if (node.childNodes.length == 1) {
+                    let { temp, } = XMLDocToCustomObj(node.firstChild);
+                    CJ.value = temp;
+                    noAttributesCJ.value = temp;
+                }
+                else {
+                    for (let child of node.childNodes) {
+                        let { temp, } = XMLDocToCustomObj(child);
+                        CJ.value.push(temp);
+                        noAttributesCJ.value.push(temp);
+                    }
+                }
+                CJ.attributes = [];
+                for (let attr of node.attributes) {
+                    let { temp, } = XMLDocToCustomObj(attr, CJ.id);
+                    CJ.attributes.push(temp);
+                    attributes.push(temp);
+                }
+                break;
+            case Node.ATTRIBUTE_NODE: // an attribute
+                return {
+                    id: lastId++,
+                    attributeKey: node.nodeName,
+                    attributeValue: node.nodeValue,
+                    parentId: id,
+                }
+            case Node.DOCUMENT_NODE: // the root of the document tree
+                return XMLDocToCustomObj(node.firstChild);
+            default:
+                console.log("unsupported xml node type: " + node + " " + node.nodeType);
+                break;
+        }
+
+        return { CJ, noAttributesCJ };
+    }
+}
