79b1b99b10d4fe4665673db56c36e2f96f413d3c
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 15:25:08 +0200

    Minimal page structure, button callbacks

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -130,7 +130,7 @@ sap.ui.define(
         let subTreeValue = {
           key: "key",
           value: "value",
-          id: lastId++,
+          id: lastElementId++,
         };
         if (Array.isArray(subTree.value)) subTree.value.push(subTreeValue);
         else {
@@ -206,7 +206,7 @@ sap.ui.define(
         let newSubTree = {
           key: subTree.key,
           value: replaceIds(subTree.value),
-          id: lastId++,
+          id: lastElementId++,
         };
 
         // add attributes
@@ -217,7 +217,7 @@ sap.ui.define(
             for (let index in originalTree.value) {
               for (let attribute of filteredAttributes) {
                 model.allAttributes.push({
-                  id: lastId++,
+                  id: lastElementId++,
                   attributeKey: attribute.attributeKey,
                   attributeValue: attribute.attributeValue,
                   parentId: newTree.id,
@@ -228,7 +228,7 @@ sap.ui.define(
           } else {
             for (let attribute of filteredAttributes) {
               model.allAttributes.push({
-                id: lastId++,
+                id: lastElementId++,
                 attributeKey: attribute.attributeKey,
                 attributeValue: attribute.attributeValue,
                 parentId: newTree.id,
@@ -398,7 +398,7 @@ sap.ui.define(
       onAddAttribute: function () {
         let selected = originalTree.getSelectedItems()[0];
         let newAttr = {
-          id: lastId++,
+          id: lastElementId++,
           attributeKey: "name",
           attributeValue: "value",
           parentId: getCustomIdFromRecord(selected),
--- controller/Management.controller.js
+++ controller/Management.controller.js
@@ -9,13 +9,58 @@ sap.ui.define(
         return Controller.extend("sap.ui.demo.walkthrough.controller.Management", {
             onInit: function () {
                 let jsonModel = new JSONModel({
-                    data: [
-                        "sus", "sas", "ses", "sis", "sos",
+                    files: [
+                        {
+                            id: 1,
+                            title: "Hey1",
+                            content: "<sus></sus>",
+                        },
+                        {
+                            id: 2,
+                            title: "Hey2",
+                            content: "<sus></sus>",
+                        },
+                        {
+                            id: 3,
+                            title: "Hey3",
+                            content: "<sus></sus>",
+                        },
+                        {
+                            id: 4,
+                            title: "Hey4",
+                            content: "<sus></sus>",
+                        }
                     ],
                 });
                 let view = this.getView();
                 view.setModel(jsonModel);
+            },
+
+            onAdd: function() {
+                
+            },
+
+            onEdit: function(event) {
+                let tile = getRecordElement(event);
+                let id = getCustomIdFromRecord(tile);
+                console.log("Editing file with id " + id);
+            },
+
+            onDownload: function(event) {
+                let tile = getRecordElement(event);
+                let id = getCustomIdFromRecord(tile);
+                console.log("Downloading file with id " + id);
             }
         });
     }
 );
+
+function getRecordElement(event) {
+    let id = event.getSource().getId();
+    return sap.ui.getCore().getElementById(id).oParent.oParent;
+}
+
+function getCustomIdFromRecord(record) {
+    if (record == undefined) return -1;
+    return record.mAggregations.content[0].mAggregations.customData[0].mProperties.value;
+}
