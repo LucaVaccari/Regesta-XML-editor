a4b9da96a9b1d86446f6ff82f9acb94686e92277
LucaVaccari <lucavaccari03@gmail.com>
2021-06-28 09:18:46 +0200

    Reformatting

--- database/config.php
+++ database/config.php
@@ -1,66 +1,59 @@
-<?php    
+<?php
 
 session_start();
 
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
 
-        //Database Connection
+//Database Connection
 
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "test1";  
-      
+$host = "localhost";
+$user = "root";
+$password = '';
+$db_name = "test1";
 
-    $con = mysqli_connect($host, $user, $password, $db_name);  
-    
-    if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error()); 
-        
-    
-    }
 
-    //$txtName = $_POST['email'];
-    //$txtEmail = $_POST['psw'];
+$con = mysqli_connect($host, $user, $password, $db_name);
 
-    //$sql = "INSERT INTO `test1` (`username`, `password`) VALUES ('$txtName', '$txtEmail')";
-    //$rs = mysqli_query($con, $sql);
+if (mysqli_connect_errno()) {
+    die("Failed to connect with MySQL: " . mysqli_connect_error());
+}
+
+//$txtName = $_POST['email'];
+//$txtEmail = $_POST['psw'];
+
+//$sql = "INSERT INTO `test1` (`username`, `password`) VALUES ('$txtName', '$txtEmail')";
+//$rs = mysqli_query($con, $sql);
 
 
 
-    $username = $_POST['email'];  
-    $password = $_POST['psw'];  
-      
-        //to prevent from mysqli injection  
-        $username = stripcslashes($username);  
-        $password = stripcslashes($password);  
-        $username = mysqli_real_escape_string($con, $username);  
-        $password = mysqli_real_escape_string($con, $password);  
+$username = $_POST['email'];
+$password = $_POST['psw'];
 
-        $sql =("SELECT *from test1 where username = '$username' and password = '$password'");  
+//to prevent from mysqli injection  
+$username = stripcslashes($username);
+$password = stripcslashes($password);
+$username = mysqli_real_escape_string($con, $username);
+$password = mysqli_real_escape_string($con, $password);
 
-        $result = mysqli_query($con,$sql);  
-        
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+$sql = ("SELECT *from test1 where username = '$username' and password = '$password'");
 
-        $active = $row['active'];
+$result = mysqli_query($con, $sql);
 
-        $count = mysqli_num_rows($result);    
-          
-        if($count > 0) {
-            $_SESSION['username'] = $username;
-            header("location: index.html");
-        }
-        else{
-            header("location: home.html");
-            
-        }
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+$active = $row['active'];
+
+$count = mysqli_num_rows($result);
+
+if ($count > 0) {
+    $_SESSION['username'] = $username;
+    header("location: index.html");
+} else {
+    header("location: home.html");
+}
 
    // if($rs)
 /*{
 	echo "Contact Records Inserted";
 }*/
-
-
-?> 
