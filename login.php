405ab66d22a8ffbc3f96a2fc1bde378b56e3bb9f
emme-ario <emme-ario@users.noreply.github.com>
2021-07-01 09:31:42 +0200

    small fixes in login form

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
                     <input type="password" placeholder="Enter Password" id="psw" name="psw" required>
                     
                     <button type="submit" id="btn" name="btn" class="login">Log In</button>
-                    <button type="submit" id="btn" name="btn" class="signup">Sign Up</button>
+                    <a href="signup.html"><button type="button" id="btn" name="btn" class="signup">Sign Up</button></a>
                     <a href="index.php"><button type="button" class="cancel">Cancel</button></a>
                 </form>
             </div>
